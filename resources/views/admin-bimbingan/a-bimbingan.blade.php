388b2999999e6279504d003f9a8375642c4e195a
Muhammad Irsyad Majid <irsyadmajid@users.noreply.github.com>
2025-07-02 16:42:50 +0700

    Minor view

--- resources/views/admin-bimbingan/a-bimbingan.blade.php
+++ resources/views/admin-bimbingan/a-bimbingan.blade.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html lang="id">
 <head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Dashboard BIMA</title>
-  <link rel="stylesheet" href="{{ asset('css/Dosen_Bimbingan/style.css') }}" />
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Dashboard BIMA</title>
+    <link rel="stylesheet" href="{{ asset('css/Dosen_Bimbingan/style.css') }}" />
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
 </head>
 <body>
     <div class="dashboard-container">
